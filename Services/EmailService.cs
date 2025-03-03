685ab776cdd384d4e95508f58c1d9fbb21b64cbb
Tom Coakley <ebuntom@gmail.com>
2025-03-03 06:52:49 -0800

    removed password security issue

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -2,28 +2,39 @@
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace zChecklist.Services
 {
     public class EmailService
     {
-        private const string SmtpServer = "smtp.gmail.com";
-        private const int SmtpPort = 587;
-        private const string SenderEmail = "zchecklistteam@gmail.com";
-        private const string SenderPassword = "MpiB00mk1n";
+
+        private readonly string _smtpServer;
+        private readonly int _smtpPort;
+        private readonly string _senderEmail;
+        private readonly string _senderPassword;
+
+        public EmailService(IConfiguration configuration)
+        {
+            var emailSettings = configuration.GetSection("EmailSettings");
+            _smtpServer = emailSettings["SmtpServer"];
+            _smtpPort = int.Parse(emailSettings["SmtpPort"]);
+            _senderEmail = emailSettings["SenderEmail"];
+            _senderPassword = emailSettings["SenderPassword"];
+        }
 
         public async Task SendForgotPasswordEmail(string recipientEmail)
         {
             try
             {
-                using (var client = new SmtpClient(SmtpServer, SmtpPort))
+                using (var client = new SmtpClient(_smtpServer, _smtpPort))
                 {
-                    client.Credentials = new NetworkCredential(SenderEmail, SenderPassword);
+                    client.Credentials = new NetworkCredential(_senderEmail, _senderPassword);
                     client.EnableSsl = true;
 
                     var mailMessage = new MailMessage
                     {
-                        From = new MailAddress(SenderEmail),
+                        From = new MailAddress(_senderEmail),
                         Subject = "Password Reset Request",
                         Body = $"Password reset information belongs here.",
                         IsBodyHtml = true
