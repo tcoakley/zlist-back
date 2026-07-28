bdfed027ea6f827120ce1c989b5c902f80292948
Tom Coakley <ebuntom@gmail.com>
2026-07-28 10:27:59 -0700

    increment version

---
+++ SqlScripts/InsertVersion_1.0.4.sql
@@ -0,0 +1,7 @@
+INSERT INTO AppVersions (Version, ReleasedAt, Notes)
+VALUES (
+	'1.0.4',
+	GETUTCDATE(),
+	'Fixed the app icon.
+- The home screen icon (Android/iOS install) and browser tab icon now show our actual green checkmark branding, instead of a gray placeholder'
+);
